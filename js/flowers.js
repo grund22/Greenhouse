a95bbaf8573784e6d027f7caad73e3b34f6c0acf
Jamie McMahan <grund23@gmail.com>
2021-11-14 14:07:15 -0600

    update

--- js/flowers.js
+++ js/flowers.js
@@ -9,3 +9,17 @@ function getRandomInt(min, max) {
 
 var name = capFirst(name1[getRandomInt(0, name1.length + 1)]) + ' ' + capFirst(name2[getRandomInt(0, name2.length + 1)]);
 return name;
+
+
+
+
+
+fetch("flowerlist.json")
+    .then(response => response.json())
+    .then(data => {
+        var flowerlist = document.getElementById('flowerlist')
+        for (var i = 0; i < data.length; i++) {
+            console.log(data[i])
+            flowerlist.innerHTML = flowerlist.innerHTML + '<p>' + data[i].title + '</p>'
+        }
+    })
