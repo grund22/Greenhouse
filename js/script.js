366f78268a335960f93eb2b98b97135d4cb6d3b8
Jamie McMahan <grund23@gmail.com>
2021-11-13 23:51:48 -0600

    update

--- js/script.js
+++ js/script.js
@@ -9,3 +9,12 @@ function newFlower() {
     var randomNumber = Math.floor(Math.random() * (flowers.length));
     document.getElementById('flowerDisplay').innerHTML = flowers[randomNumber];
 }
+fetch("flowerlist.json")
+    .then(response => response.json())
+    .then(data => {
+        var flowerlist = document.getElementById('flowerlist')
+        for (var i = 0; i < data.length; i++) {
+            console.log(data[i])
+            flowerlist.innerHTML = flowerlist.innerHTML + '<p>' + data[i].title + '</p>'
+        }
+    })
